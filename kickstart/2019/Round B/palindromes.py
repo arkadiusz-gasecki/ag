adca4ae51c0b8337a5a9d39f2ba1d21e83d602b7
arkadiusz-gasecki <60004330+arkadiusz-gasecki@users.noreply.github.com>
2020-02-28 19:55:00 +0100

    Update palindromes.py

--- kickstart/2019/Round B/palindromes.py
+++ kickstart/2019/Round B/palindromes.py
@@ -1,30 +1,39 @@
-#only first data set
+from collections import defaultdict
 
-def is_palindromic(block):
-    freq = {}
-    for b in block:
-        freq[b] = freq.get(b,0) + 1
-        odd_values = 0
-    for k,v in freq.items():
-        if v % 2 != 0:
-            odd_values += 1
-            if odd_values > 1:
-                return 0
-    return 1
+def calc_freq(block):
+    freqs = defaultdict(int)
+    
+    for i in range(len(block)):
+        freqs[block[i]] += 1
+        total_freqs[i] = dict(freqs)
+    
+def is_palindromic(L, R):
+    odd_values = 0
+    R_freqs = total_freqs[R]
+    L_freqs = total_freqs[L-1]
+    for char,r_cnt in R_freqs.items():
+        odd_values += ( (r_cnt - L_freqs.get(char,0)) % 2 )
+        if odd_values > 1:
+            return 0 
+    return 1 
 
 T = int(input())
 answers = {}
 
 for t in range(0,T):
+    total_freqs = {}
+    total_freqs[-1] = defaultdict(int)
     N,Q = map(int,input().split())
     block = input()
-    for q in range(0,Q):
-        L,R = map(int,input().split())
-        #correct L and R to have actual index of the string
-        #R does not need to be corrected as it will be used in slicing
-        L -= 1
-        #take substring
-        answers[t] = answers.get(t,0) + is_palindromic(block[L:R])
 
-for i in range(0,len(answers)):
+    calc_freq(block)
+    #print(total_freqs)
+    #print(total_freqs)
+    answers[t] = 0
+    for q in range(0,Q):
+        L,R = map( lambda k: int(k)-1,input().split())
+        #take substring
+        answers[t] += is_palindromic(L,R)
+
+for i in range(len(answers)):
     print("Case #{}: {}".format(i+1,answers[i]))
